6de96100bfb14d97d31a39f032ad0a838fa19f94
Aftab3008 <shaikaftab005@gmail.com>
2025-06-28 17:58:15 +0530

    Add express router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
 import { startEmailWorker } from "./workers/otp.workers.js";
+import express from "express";
+
 dotenv.config();
 
-if (process.env.WORKER === "true") {
-  await startEmailWorker();
-} else {
-  console.log("Set WORKER=true to start the Email worker");
-}
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Email Worker is running");
+});
+
+await startEmailWorker();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
